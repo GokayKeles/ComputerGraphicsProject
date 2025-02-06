81e81e0ce6723f3b2bd39f32f5310c1c520b2a5a
Gökay <1210606803@nku.edu.tr>
2025-02-06 18:29:47 +0300

    .

--- ComputerGraphicsProject/Form1.Designer.cs
+++ ComputerGraphicsProject/Form1.Designer.cs
@@ -28,46 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            SuspendLayout();
-            // 
-            // button2
-            // 
-            button2.Location = new Point(70, 28);
-            button2.Name = "button2";
-            button2.Size = new Size(147, 26);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(315, 28);
-            button3.Name = "button3";
-            button3.Size = new Size(147, 26);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(561, 28);
-            button4.Name = "button4";
-            button4.Size = new Size(147, 26);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -76,8 +36,6 @@
 
         #endregion
 
-        private Button button2;
-        private Button button3;
-        private Button button4;
+
     }
 }
